bc85813d47fc4533584592f79772521d690a1d86
menna saeed <mennasaiid16@gmail.com>
2024-07-11 02:21:18 +0300

    initial commit

---
+++ Back end/Game/Game.Services/IScoreService.cs
@@ -0,0 +1,17 @@
+﻿using Game.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Services
+{
+    public interface IScoreService
+    {
+        public List<GetScoreDto> GetAll();
+        public GetScoreDto GetOneByUsername(string username);
+        public bool Create(GetScoreDto e);
+       // public bool Update(int Id, GetScoreDto e);
+    }
+}
